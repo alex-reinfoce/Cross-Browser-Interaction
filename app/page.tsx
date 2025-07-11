ff0ec2826d8a4dc1fb06d38c4f9058fe93390758
alex-reinforce <alex.lichangnan@gmail.com>
2025-07-11 18:34:19 +0800

    Refactor Home component layout and add OtherPage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/button";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button />
+    <div>
+      <div className="w-[100px] h-[100px] bg-amber-300" />
+      <button className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">
+        发送到另一个页面
+      </button>
     </div>
   );
 }
