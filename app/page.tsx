42229f4238ba561715ef0e26ed0cbdbaff963990
alex-reinforce <alex.lichangnan@gmail.com>
2025-07-11 18:48:49 +0800

    Add "use client" directive to OtherPage and Home components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 export default function Home() {
   return (
-    <div>
+    <div className="w-screen h-screen flex flex-col items-center pt-[100px]">
       <div className="w-[100px] h-[100px] bg-amber-300" />
       <button className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">
         发送到另一个页面
